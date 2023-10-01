c68b57dbb9103829b2fad7124f9c99d5a6534799
armj505 <ahmad.almohanna@outlook.com>
2023-10-01 20:27:51 +0300

    task completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import products from "./products";
+
+const productList = products.map((book) => {
+  return (
+    <div className="Card">
+      <img src={book.img}></img>
+      <h2>{book.name}</h2>
+      <h4>{book.price.toFixed(3)} KD</h4>
+    </div>
+  );
+});
 
 function App() {
   return (
     <div className="App">
-      <div className="Title-Header">
+      <div>
         <h1>Ahmad Publishing House</h1>
         <h3>A creatively and editorially independent publishing house</h3>
       </div>
       <div>
-        <img src="https://img1.wsimg.com/isteam/stock/86837/:/rs=h:1000,cg:true,m"></img>
+        <img
+          className="headerimg"
+          src="https://img1.wsimg.com/isteam/stock/86837/:/rs=h:1000,cg:true,m"
+        ></img>
       </div>
+      <div className="bookCard">{productList}</div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    name: "To Kill a Mockingbird",
+    price: 4,
+    img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRO7Te2MLyEC2TPkOt7Ghh_SpYdEyuYUsznDwdc-0qhy-Ju4v0I",
+  },
+  {
+    name: "The Great Gatsby",
+    price: 3,
+    img: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTMzFauFHVQTkklojHoIspzFxRzx_G-p5VgKg-XhmCiwUz2wnq1",
+  },
+  {
+    name: "Placeholder",
+    price: 999,
+    img: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Placeholder_book-ar.svg",
+  },
+];
+
+export default products;
